035e75f44646baaaca22e062c0a341e24fba8f6a
Bayejid Hawlader <bayejidhawlader@gmail.com>
2022-11-08 21:17:32 +0600

    first commit

---
+++ src/Pages/Blog/Blog.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div>
+      {/* 01 */}
+      <div
+        tabIndex={0}
+        className="collapse collapse-open border border-base-300 bg-base-100 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
+          Difference between SQL and NoSQL
+        </div>
+        <div className="collapse-content">
+          <p>
+            SQL is the programming language used to interface with relational
+            databases. (Relational databases model data as records in rows and
+            tables with logical links between them). NoSQL is a class of DBMs
+            that are non-relational and generally do not use SQL.
+          </p>
+        </div>
+      </div>
+
+      {/* 01 */}
+      <div
+        tabIndex={0}
+        className="collapse collapse-open border border-base-300 bg-base-100 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
+          Difference between SQL and NoSQL
+        </div>
+        <div className="collapse-content">
+          <p>
+            SQL is the programming language used to interface with relational
+            databases. (Relational databases model data as records in rows and
+            tables with logical links between them). NoSQL is a class of DBMs
+            that are non-relational and generally do not use SQL.
+          </p>
+        </div>
+      </div>
+
+      {/* 01 */}
+      <div
+        tabIndex={0}
+        className="collapse collapse-open border border-base-300 bg-base-100 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
+          Difference between SQL and NoSQL
+        </div>
+        <div className="collapse-content">
+          <p>
+            SQL is the programming language used to interface with relational
+            databases. (Relational databases model data as records in rows and
+            tables with logical links between them). NoSQL is a class of DBMs
+            that are non-relational and generally do not use SQL.
+          </p>
+        </div>
+      </div>
+
+      {/* 01 */}
+      <div
+        tabIndex={0}
+        className="collapse collapse-open border border-base-300 bg-base-100 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
+          Difference between SQL and NoSQL
+        </div>
+        <div className="collapse-content">
+          <p>
+            SQL is the programming language used to interface with relational
+            databases. (Relational databases model data as records in rows and
+            tables with logical links between them). NoSQL is a class of DBMs
+            that are non-relational and generally do not use SQL.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="">
+      <div
+        className="hero"
+        style={{
+          backgroundImage: `url("https://i.postimg.cc/tT8zB142/Banner-10.jpg")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content py-48">
+          <div className="max-w-md">
+            {/* <h1 className="mb-5 text-5xl font-bold">Hello there</h1> */}
+            <p className="mb-5 text-xl text-white">
+              We advocate for employee rights and help companies comply with
+              workplace laws.
+            </p>
+            <button className="btn btn-primary">Learn More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
